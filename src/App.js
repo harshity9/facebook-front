f5ddfe8c98ce59ee77c13f0276fa08e54cc514b4
harshity9 <harshityadav0927@gmail.com>
2023-02-21 13:07:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='login-page'>
+    <div className='box'>
+    <div className='left-side'> <div><img className='image' src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg'></img>
+</div>     
+<h2 className='text'>
+ Facebook helps you connect and share with the people in your life.
+ </h2>
+</div>
+      <div className='login-layout'>
+      <div>  <input className='email form-control' placeholder='Email address or phone number'></input></div>
+       <div > <input className='password form-control' placeholder='Password'></input></div>
+       <div>
+        <button className='login-btn btn btn-primary'>Log in</button></div>
+       <div> <a href='link'>Forgotten password?</a></div>
+<div className='line'></div>
+      <div>
+        <button className='create-btn btn btn-success'> Create new account</button></div>
+      </div>
+      </div>
+   
+    </div> 
+         </div>
   );
 }
 
